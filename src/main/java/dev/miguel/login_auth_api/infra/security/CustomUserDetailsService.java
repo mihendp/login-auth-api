e41267a620e74dee7c10ef4c8a240def0a3b73ea
Miguel Henrique Duran <miguelhdp009@gmail.com>
2024-07-24 11:29:50 -0300

    Create CustomUserDetailsService class

---
+++ src/main/java/dev/miguel/login_auth_api/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package dev.miguel.login_auth_api.infra.security;
+
+import dev.miguel.login_auth_api.domain.user.User;
+import dev.miguel.login_auth_api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getSenha(), new ArrayList<>());
+    }
+}
